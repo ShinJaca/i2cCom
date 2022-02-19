f718e29d1b61ccf2de8e2b21d69338ad84ec842d
Sergio Pugliesi <sergiopugli@gmail.com>
2022-02-18 22:41:05 -0300

    init sensor

--- exemples/MPU6000_temp.cpp
+++ exemples/MPU6000_temp.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <i2cCom.h>
 
 #define MPUADDR 0x68
 #define REG_TEMP 0x41
+#define REG_INIT 0x6B
 #define DELAY_TIME 50
 
 i2cCom dev(MPUADDR);
@@ -11,6 +13,8 @@ void setup()
 {
     Serial.begin(9600);
     Serial.println("MPU6000 Temperature log");
+    Wire.begin();
+    dev.writeOneByte(REG_INIT, 0); //init MPU
 }
 
 void loop()
